8817fe28fe63a905ff7345c45a9159873d04401e
ShirlyB-24-tech <160262117+ShirlyB-24-tech@users.noreply.github.com>
2024-05-09 20:53:08 +0200

    adding api key

--- src/Weather.js
+++ src/Weather.js
@@ -31,43 +31,41 @@ export default function Weather(props) {
     setCity(event.target.value);
   }
 
-    function search() {
-        const apiKey = "ad793a6d772939c31783de5822791acf";
+  function search() {
+    const apiKey = "ad793a6d772939c31783de5822791acf";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
-    
-  }
+}
 
-  if (weatherData.ready) {
-    return (
-      <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-                onChange={handleCityChange}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
+if (weatherData.ready) {
+  return (
+    <div className="Weather">
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoFocus="on"
+              onChange={handleCityChange}
+            />
           </div>
-        </form>
-        <WeatherInfo data={weatherData} />
-        <WeatherForecast coordinates={weatherData.coordinates} />
-      </div>
-    );
-  } else {
-    search();
-    return "Loading...";
-  }
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <WeatherInfo data={weatherData} />
+      <WeatherForecast coordinates={weatherData.coordinates} />
+    </div>
+  );
+} else {
+  search();
+  return "Loading...";
 }
